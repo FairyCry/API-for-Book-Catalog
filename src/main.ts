1fa6e940e89ccee7c56d58b0ee522926b6039146
FairyCry <zxcbogdan933@gmail.com>
2026-07-13 00:41:51 +0300

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  
   app.enableCors({
-    origin: 'http://localhost:5173', // адрес твоего vite фронта
+    origin: 'http://localhost:5173',
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('Book Catalog API')
+    .setDescription('API для книжного каталога')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
